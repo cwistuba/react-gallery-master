7eae6506c696c040899d28cbefa37a65264c7c04
Caleb Wistuba <calebwistuba@gmail.com>
2020-04-11 13:07:45 -0500

    added 2 additional images

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,22 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: "images/goat_small.jpg",
+    description: "Photo of a goat taken at Glacier National Park.",
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: "images/golf_ball.jpg",
+    description: "Photo of my favorite Golf Ball Brand",
+    likes: 0,
+  },
+  {
+    id: 3,
+    path: "images/kc_chiefs.jpg",
+    description: "Photo of the best NFL Football Team!!",
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
