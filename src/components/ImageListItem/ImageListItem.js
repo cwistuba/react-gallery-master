dd4967c06163032d4a33bfe5a2e74524682da06a
Caleb Wistuba <calebwistuba@gmail.com>
2020-04-11 15:03:21 -0500

    completed put route

--- src/components/ImageListItem/ImageListItem.js
+++ src/components/ImageListItem/ImageListItem.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import "./ImageListItem.css";
+
 class ImageListItem extends Component {
   render() {
+    let buttonElement = <div>0</div>;
+    if (!this.props.item.likes) {
+      buttonElement = (
+        <button onClick={this.props.updateLikes(this.props.item.id)}>
+          Like
+        </button>
+      );
+    }
+
     return (
       <div className="imageListItem-item">
         <img src={this.props.item.path} />
-        <p>
-          {" "}
-          <button onClick={this.props.updateLikes(this.props.item.id)}>
-            Like
-          </button>
-        </p>
+        <p> {buttonElement}</p>
       </div>
     );
   }
